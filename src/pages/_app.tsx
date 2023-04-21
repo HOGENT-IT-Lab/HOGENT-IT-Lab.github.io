54673d3fe7e5fb940219168d08bad819c897f30b
TieProost <100168771+TieProost@users.noreply.github.com>
2023-04-21 11:05:43 +0200

    add auth0 provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import Layout from '@/components/Layout';
+import MyAuth0Provider from '@/contexts/MyAuth0Provider';
 import '@/styles/globals.scss';
 
 type Props = AppProps & { Component: React.ReactNode };
 
 export default function MyApp({ Component, pageProps }: Props) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<MyAuth0Provider>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</MyAuth0Provider>
 	);
 }
